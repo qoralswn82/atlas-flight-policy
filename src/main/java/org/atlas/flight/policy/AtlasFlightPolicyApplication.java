a31cafb79755450284c5d556d0b9a41ca0c3e3c7
백민주 <mj.baek@okestro.com>
2026-06-17 13:33:35 +0900

    initial : initial atlas-flight-policy project

---
+++ src/main/java/org/atlas/flight/policy/AtlasFlightPolicyApplication.java
@@ -0,0 +1,13 @@
+package org.atlas.flight.policy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasFlightPolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasFlightPolicyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/atlas/flight/policy/AtlasFlightPolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atlas.flight.policy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasFlightPolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
